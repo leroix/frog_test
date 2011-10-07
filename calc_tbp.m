74428b653595c9eac31b335bc704d2346523e8d5
Justin R <leroix08@gmail.com>
2011-10-06 20:51:48 -0400

    added more tools

---
+++ calc_tbp.m
@@ -0,0 +1,39 @@
+function [tbp_rms, sig_t, sig_w] = calc_tbp(et, ew)
+%calc_tbp - calculates the root mean square time-bandwidth product of Et
+%
+% Inputs:
+%    et - complex electric field vs. time
+%    ew - complex electric field vs. frequency
+%
+% Outputs:
+%    tbp_rms - root mean square time-bandwidth product of Et
+%    sig_t - normalized standard deviation of the electric field in time
+%    sig_w - normalized standard deviation of the electric field in
+%            frequency
+%
+
+% Author: Justin Ratner
+% Georgia Institute of Technology
+% email address: jtratner@gatech.edu  
+% Website: http://frog.gatech.edu
+% October 2011; Last revision: 05-Oct-2011
+
+n = length(et);
+t = linspace(-n/2, n/2, n);
+w = linspace(-pi, pi, n);
+intensity_t = abs(et).^2;
+intensity_t = intensity_t / trapz(intensity_t);
+intensity_w = abs(ew).^2;
+intensity_w = intensity_w / (trapz(intensity_w)*2*pi/n);
+
+mean_t = trapz(t .* intensity_t);
+mean_w = trapz(w .* intensity_w) * 2*pi/n;
+mean_t2 = trapz(t.^2 .* intensity_t);
+mean_w2 = trapz(w.^2 .* intensity_w) * 2*pi/n;
+
+t_var = mean_t2 - mean_t^2;
+w_var = mean_w2 - mean_w^2;
+
+tbp_rms = sqrt(t_var) * sqrt(w_var);
+sig_t = sqrt(t_var) / n;
+sig_w = sqrt(w_var) / (2*pi);
---
+++ rand_pulse.m
@@ -0,0 +1,71 @@
+function [et, ew, tbp_rms] = rand_pulse(tbp, n)
+%rand_pulse - returns a random pulse of a given time-bandwidth product
+%
+% Syntax:  [et, ew, tbp_rms] = rand_pulse(tbp, N)
+%
+% Inputs:
+%    tbp - Desired time-bandwidth product.
+%    n - (OPTIONAL) size of the 1xN array of the pulse. Function can choose
+%        and array size automatically.
+%
+% Outputs:
+%    et - complex electric field vs. time
+%    ew - complex electric field vs. frequency (FFT of Et)
+%    tbp_rms - root mean square time-bandwidth product. TBP +/- 5%
+%
+
+% Author: Justin Ratner
+% Georgia Institute of Technology
+% email address: jtratner@gatech.edu  
+% Website: http://frog.gatech.edu
+% October 2011; Last revision: 05-Oct-2011
+% REQUIREMENTS: calc_tbp.m
+
+% choose the best n or take the user's input if available
+debug = 1;
+
+n1 = 2^ceil(log2(32*(tbp/.5)^1));
+if n1 > 16384
+    n1 = 16384;
+end
+if nargin == 1
+    n = n1;
+end
+
+tbp_rms = inf;
+sig_t = 0;
+sig_w = inf;
+x = linspace(-1,1,n);
+width_t = 0.6;
+width_w = 0.6;
+k = 1;
+while abs(tbp_rms - tbp)/tbp > 0.05 || ...
+      abs(sig_t - sig_w)/sig_w > 0.05
+    envelope_t = exp(-2*log(2)*(x / width_t).^2);
+    envelope_w = exp(-2*log(2)*(x / width_w).^2);
+    
+    et = rand(1, n) .* exp(i*2*pi*rand(1, n));
+    et = et .* envelope_t;
+    ew = fftc(et) .* envelope_w;
+    et = ifftc(ew);
+    [tbp_rms, sig_t, sig_w] = calc_tbp(et, ew);
+    
+    if abs(tbp_rms - tbp)/tbp > 0.10
+        factor = 1 + sign(tbp - tbp_rms)*0.08;
+        width_t = width_t * factor;
+        width_w = width_w * factor;
+    end
+    
+    if abs(sig_t - sig_w)/sig_w > 0.10
+        width_t = width_t * (1 + sign(sig_w - sig_t)*0.08);
+    end
+    
+    k = k + 1;
+    if k > 1000
+        error('Error: could not generate a pulse for the given parameters.')
+    end
+end
+
+if debug
+    fprintf('k = %s \n', num2str(k))
+end
