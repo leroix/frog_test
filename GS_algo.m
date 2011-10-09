75133e25dd069560c8e1c45b45ba34712c26ea24
Justin R <leroix08@gmail.com>
2011-10-08 22:17:40 -0400

    added gerchberg-saxton algorithm function

---
+++ GS_algo.m
@@ -0,0 +1,27 @@
+function phiw = GS_algo(It, Sw)
+
+At = center(quickscale(sqrt(It)),'max');
+Aw = center(quickscale(sqrt(Sw)),'max');
+N = length(At);
+err = ones(1,N);
+
+Et = ifftc(Aw);
+for k = 1:400
+    Et = At .* Et./abs(Et);
+    Ew = fftc(Et);
+    Ew = Aw .* Ew./abs(Ew);
+    Et = ifftc(Ew);
+    
+    At_curr = center(quickscale(abs(Et)),'max');
+    curr_err = sqrt(trapz((At-At_curr).^2)/N);
+    if curr_err < min(err)
+        phiw_best = angle(Ew);
+    end
+    err(k) = curr_err;
+    
+    if 1
+        plot(err)
+    end
+end
+
+phiw = phiw_best;
